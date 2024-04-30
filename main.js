da6a89b2d87003fc18e5138af7ac48d9077926a1
devflaco <npressure00@gmail.com>
2024-04-30 19:40:05 -0300

    removed useful comments

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ function fetchDataAndRender() {
   fetch('https://rickandmortyapi.com/api/character')
     .then(response => response.json())
     .then((responseData) => {
-      data = responseData; // Almacenar los datos globalmente
-      renderCards(data.results); // Renderizar las tarjetas al cargar los datos
+      data = responseData;
+      renderCards(data.results);
     })
     .catch(error => console.error('Error al obtener los datos:', error));
 }
@@ -43,14 +43,14 @@ function searchB() {
   const search = searchBar.value.trim().toLowerCase();
 
   if (search === '') {
-    renderCards(data.results); // Si la búsqueda está vacía, renderizar todas las tarjetas
+    renderCards(data.results);
     return;
   }
 
   const filteredChar = data.results.filter(character => character.name.toLowerCase().includes(search));
 
   if (filteredChar.length > 0) {
-    renderCards(filteredChar); // Renderizar las tarjetas filtradas
+    renderCards(filteredChar);
   } else {
     const message = document.createElement('p');
     message.textContent = 'No se encontraron resultados';
@@ -61,7 +61,7 @@ function searchB() {
 // Event listener para el evento "keyup" en la barra de búsqueda
 searchBar.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
-    searchB(); // Llamar a la función de búsqueda al presionar Enter
+    searchB();
   }
 });
 
@@ -88,74 +88,10 @@ const showFilterButtons = () => {
     button.textContent = specie;
     button.addEventListener('click', () => {
       const filteredChar = data.results.filter(character => character.species === specie);
-      renderCards(filteredChar); // Aquí es donde deberíamos llamar a renderCards() con los datos filtrados
+      renderCards(filteredChar);
     });
     speciesButtonsContainer.appendChild(button);
   });
   speciesButtonsContainer.style.display = "block";
-}
-// const productContent = document.getElementById('cardsCont');
-// const searchBar = document.getElementById('searchBar');
-// const filterButton = document.getElementById('filterButton');
-// const categoryButtonsContainer = document.getElementById('categoryButtons');
+  };
 
-// fetch('https://rickandmortyapi.com/api/character')
-//   .then(response => response.json())
-//   .then((data) => {
-//     setTimeout(() => { 
-//       data.results.forEach((character) => {
-//           const card = document.createElement('div');
-//           card.className = 'card';
-//           card.innerHTML = `
-//           <div class="card__content">
-//           <div class="imgChar">
-//               <img src="${character.image} " alt="">
-//           </div>
-//           <h2>${character.name}</h2>
-//           <p><i class='bx bx-stats colorLet'></i>Status : ${character.status}</p>
-//           <p><i class='bx bxs-spa colorLet'></i>Especie : ${character.species} </p>
-//           <p><i class='bx bx-male-female colorLet'></i>Género : ${character.gender} </p>
-//       </div>`;
-//           productContent.appendChild(card);
-//       })
-//     }, 300);
-//   })
-//   .catch(error => console.error('Error al obtener los datos:', error));
-
-// const searchB = () => {
-
-//   productContent.innerHTML = '';
-
-//   const search = searchBar.ariaValueMax.trim().toLowerCase();
-//   const filteredChar = data.results.filter(character => character.name.toLowerCase().includes(search));
-
-//   if (filteredChar.length > 0) {
-
-//     filteredChar.forEach(character => {
-//       const card = document.createElement('div');
-//       card.className = 'card';
-//           card.innerHTML = `
-//           <div class="card__content">
-//           <div class="imgChar">
-//               <img src="${character.image} " alt="">
-//           </div>
-//           <h2>${character.name}</h2>
-//           <p><i class='bx bx-stats colorLet'></i>Status : ${character.status}</p>
-//           <p><i class='bx bxs-spa colorLet'></i>Especie : ${character.species} </p>
-//           <p><i class='bx bx-male-female colorLet'></i>Género : ${character.gender} </p>
-//       </div>`;
-//       productContent.appendChild(card);
-//     });
-//   } else {
-//     const message = document.createElement('p');
-//     message.textContent = 'No se encontraron resultados';
-//     productContent.appendChild(message);
-//   }
-// };
-
-// searchBar.addEventListener("keyup", (event) => {
-//   if (event.key === "Enter") {
-//     searchB();
-//   }
-// });
-  
